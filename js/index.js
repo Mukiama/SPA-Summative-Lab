4b562a315d0e39bd21070ea24b56a95152c88178
Mukiama Mwereria <mukiamamwereria@gmail.com>
2026-04-12 23:26:14 +0300

    Third Commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
-fetch("https://api.dictionaryapi.dev/api/v2/entries/en/<apple>")
+
+function searchWord() {
+const word = document.querySelector("#wordInput").value;
+
+fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    
 .then((response) => response.json())
-.then((data) => dictionary(data));
+.then((data) => displayResult(data))
+.catch((error) => console.error("Error fetching data:", error));
+}
+
+
+function displayResult(data) {
+    const word = data[0].word;
+    const meaning = data[0].meanings[0].definitions[0].definition;
+    const example = data[0].meanings[0].definitions[0].example;
+    const audio = data[0].phonetics[0]?.audio;
+
+    document.querySelector("#word").textContent = word;
+    document.querySelector("#meaning").textContent = meaning;
+    document.querySelector("#example").textContent = example;
+    const audioBtn = document.querySelector("#play-audio");
+
+    if (audio) {
+        audioBtn.onclick = () => {
+             new Audio(audio).play();
+            
+        };
+    }
+}
+document.querySelector("#search-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchWord();
+})
